9349ced906e94874211ced83d54395e6546234d5
20220270 <20220270@ricaldone.edu.sv>
2024-03-30 09:04:02 -0600

    commit. Imagenes agregagad e index agregado

---
+++ resources/js/footerPublico.js
@@ -0,0 +1,28 @@
+const FOOTER = document.querySelector('footer');
+FOOTER.innerHTML = `
+<div class="text-center">
+            <h2 class="mt-2">Nuestras redes sociales</h2>
+        </div>
+
+        <div class="container text-center iconos mt-4">
+            <div class="row">
+                <div class="col">
+                    <img src="../../resources/images/logoFacebook.png" alt="..." width="30px" height="30px">
+                    La Academia BP
+                </div>
+                <div class="col">
+                    <img src="../../resources/images/logoInsta.png" alt="..." width="30px" height="30px">
+                    academiafc.sv
+                </div>
+                <div class="col">
+                    <h6 class="d-block">No olvides utlizar el</h6>
+                    <h6>#SomosAcademia</h6>
+                </div>
+            </div>
+        </div>
+        <div class="row filaCopy">
+            <div class="col-md-12 text-center mt-2">
+                <p>&copy; 2024 Copyright <a href="#" class="text-white">Academia BP</a></p>
+            </div>
+        </div>
+`;
