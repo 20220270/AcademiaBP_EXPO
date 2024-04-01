feba164d65faef5dbac4f0fa5b225b18f7924486
20220270 <20220270@ricaldone.edu.sv>
2024-03-31 23:05:20 -0600

    Perfil terminado

--- controllers/public/perfil.js
+++ controllers/public/perfil.js
@@ -9,11 +9,51 @@ document.addEventListener("DOMContentLoaded", function() {
         const inputs = document.querySelectorAll(".formsPerfil");
 
         inputs.forEach(function(input) {
-            if (input.disabled) {
-                input.disabled = false; // Habilitar input
-            } else {
-                input.disabled = true; // Deshabilitar input
+            if (input.disabled && input.id !== "campoContraseña") {
+                input.disabled = false; // Habilitar input si está deshabilitado y no es el campo de contraseña
+            } else if (input.id !== "campoContraseña") {
+                input.disabled = true; // Deshabilitar input si está habilitado y no es el campo de contraseña
             }
         });
     });
 });
+
+//pop over
+document.getElementById('botonActualizarDatos').addEventListener('mouseenter', function () {
+    var popover = new bootstrap.Popover(this, {
+        title: 'Editar datos',
+        content: 'Haz clic aquí para editar o guardar tus datos. La contraseña no puedes editarla aquí',
+        placement: 'top',
+        trigger: 'manual',
+        boundary: 'viewport'
+    });
+    popover.show();
+});
+
+document.getElementById('botonActualizarDatos').addEventListener('mouseleave', function () {
+    var popover = bootstrap.Popover.getInstance(this);
+    if (popover) {
+        popover.hide();
+    }
+});
+
+//pop over
+document.getElementById('botonActualizarContrasena').addEventListener('mouseenter', function () {
+    var popover = new bootstrap.Popover(this, {
+        title: 'Editar contraseña',
+        content: 'Haz clic aquí para editar tu contraseña.',
+        placement: 'top',
+        trigger: 'manual',
+        boundary: 'viewport'
+    });
+    popover.show();
+});
+
+document.getElementById('botonActualizarContrasena').addEventListener('mouseleave', function () {
+    var popover = bootstrap.Popover.getInstance(this);
+    if (popover) {
+        popover.hide();
+    }
+});
+
+
