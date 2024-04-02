ff0abe2c164908884fdf62de720b9d33eea60c0b
20220270 <20220270@ricaldone.edu.sv>
2024-04-01 21:27:01 -0600

    modal para valorar productos terminada, elimine archivos innecesarios y pasé codigos de esos archivos innecesarios a los js que ya teniamos

--- controllers/public/compras.js
+++ controllers/public/compras.js
@@ -33,3 +33,67 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+//Estrellas
+
+//Almacenamos todos los objetos que tienen la clase star
+const stars = document.querySelectorAll('.star');
+
+//Aquí irá la variable para almacenar la cantidad de estrellas seleccionadas
+const ratingText = document.getElementById('texto');
+
+//Función para pintar y despintar las estrellas a las que se les de click
+stars.forEach(function(star, index){
+    star.addEventListener('click', function(){
+        //Aquí las pinta dependiendo el index de la estrella
+        for(let i = 0; i<=index; i++)
+        {
+            stars[i].classList.add('checked');
+        }
+        //Aquí las despinta dependiendo el index de la estrella
+        for(let i = index + 1; i< stars.length; i++)
+        {
+            stars[i].classList.remove('checked');
+        }
+        //Se almacena la cantidad de estrellas seleccionadas
+        ratingText.value = (index + 1).toString();
+    })
+});
+
+//Popup para la imagen 1
+document.getElementById('botonCerrar').addEventListener('mouseenter', function () {
+    var popover = new bootstrap.Popover(this, {
+        title: 'Cerrar ventana',
+        content: 'Haz clic aquí para cerrar esta ventana, o haz clic en la imagen de nuestro logo para cerrar.',
+        placement: 'top',
+        trigger: 'manual',
+        boundary: 'viewport'
+    });
+    popover.show();
+});
+
+document.getElementById('botonCerrar').addEventListener('mouseleave', function () {
+    var popover = bootstrap.Popover.getInstance(this);
+    if (popover) {
+        popover.hide();
+    }
+});
+
+//Popup para la imagen 2
+document.getElementById('botonCerrar2').addEventListener('mouseenter', function () {
+    var popover = new bootstrap.Popover(this, {
+        title: 'Cerrar ventana',
+        content: 'Haz clic aquí para cerrar esta ventana, o haz clic en la imagen de comentarios para cerrar.',
+        placement: 'top',
+        trigger: 'manual',
+        boundary: 'viewport'
+    });
+    popover.show();
+});
+
+document.getElementById('botonCerrar2').addEventListener('mouseleave', function () {
+    var popover = bootstrap.Popover.getInstance(this);
+    if (popover) {
+        popover.hide();
+    }
+});
