49a6c7469e0b22d914c52af1eafd3339bbc6c5c8
Ander Figueroa <20220105@ricaldone.edu.sv>
2024-04-10 11:57:13 -0600

    navbar definitivo :)

--- controllers/admin/inicio.js
+++ controllers/admin/inicio.js
@@ -2,88 +2,95 @@ const contenedorMenu = document.querySelector('#contenedor-menu');
 
 const menuHTML = `
 <!-- Menú lateral -->
-<div class="container-fluid">
-        <div class="row flex-nowrap">
-            <div class="bg-black col-auto col-md-3 min-vh-100 col-lg-3 ">
-                <div class="bg-black p-2">
-                    <a class="d-flex text-decoration-none mt-1 align-items-center ">
-                        <span class="fs-4 d-none d-sm-inline"><img src="/resources/images/logoAcademiaBP.png" alt="Logo"width="120px" height="125px" style="margin-left: 90px;">
-                        </span>
-                    </a>
-                    <ul class="nav nav-pills flex-column">
+<nav class="navbar navbar-dark  bg-black fixed-top ">
+        <div class="container-fluid">
+            <img src="/resources/images/logoAcademiaBP.png" alt="Logo" width="60px" height="60px">
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+                aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar"
+                aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header bg-black">
+                    <img src="/resources/images/logoAcademiaBP.png" class="position-absolute top-0 start-50 translate-middle-x" alt="Logo" width="60px" height="60px">
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body bg-black">
+                    <form class="d-flex mt-3" role="search">
+                        <input class="form-control me-2 mt-3" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3 mt-3">
                         <li class="nav-item ">
                             <a href="" class="nav-link active bg-black mt-3" aria-current="page">
-                                <i class="bi bi-house-door fs-4 me-2"></i><span class="ms-1 d-none d-sm-inline">Home</span>
+                                <i class="bi bi-house-door fs-4 me-2"></i><span
+                                    class="ms-1 d-none d-sm-inline text-white">Home</span>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="" class="nav-link active bg-black mt-3">
-                                <i class="bi bi-sliders fs-4 me-2" ></i><span class="ms-1 d-none d-sm-inline">Categoria de productos</span>
+                                <i class="bi bi-sliders fs-4 me-2"></i><span
+                                    class="ms-1 d-none d-sm-inline text-white">Categoria
+                                    de productos</span>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="" class="nav-link active bg-black mt-3">
-                                <i class="fa-solid fa-money-bills fs-4 me-2"></i><span class="ms-1 d-none d-sm-inline">productos</span>
+                                <i class="fa-solid fa-money-bills fs-4 me-2"></i><span
+                                    class="ms-1 d-none d-sm-inline text-white">productos</span>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="" class="nav-link active bg-black mt-3">
-                                <i class="fas fa-hand-holding-usd fs-4 me-2"></i><span class="ms-1 d-none d-sm-inline">Pagos de mensualidad</span> 
+                                <i class="fas fa-hand-holding-usd fs-4 me-2"></i><span
+                                    class="ms-1 d-none d-sm-inline text-white">Pagos de mensualidad</span>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="" class="nav-link active bg-black mt-3">
-                                <i class="bi bi-person-vcard fs-4 me-2"></i><span class="ms-1 d-none d-sm-inline">Alumnos</span>
+                                <i class="bi bi-person-vcard fs-4 me-2"></i><span
+                                    class="ms-1 d-none d-sm-inline text-white">Alumnos</span>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="" class="nav-link active bg-black mt-3">
-                                <i class="bi bi-journal-text fs-4 me-2"></i><span class="ms-1 d-none d-sm-inline">Inscripciones</span>
+                                <i class="bi bi-journal-text fs-4 me-2"></i><span
+                                    class="ms-1 d-none d-sm-inline text-white">Inscripciones</span>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="" class="nav-link active bg-black mt-3">
-                                <i class="fas fa-chalkboard-teacher fs-4 me-2"></i><span class="ms-1 d-none d-sm-inline">Profesores</span>
+                                <i class="fas fa-chalkboard-teacher fs-4 me-2"></i><span
+                                    class="ms-1 d-none d-sm-inline text-white">Profesores</span>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="" class="nav-link active bg-black mt-3">
-                                <i class="fa-solid fa-table-cells fs-4 me-2"></i><span class="ms-1 d-none d-sm-inline">Categoria de alumnos</span>
+                                <i class="fa-solid fa-table-cells fs-4 me-2"></i><span
+                                    class="ms-1 d-none d-sm-inline text-white">Categoria de alumnos</span>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="" class="nav-link active bg-black mt-4">
-                                <i class="bi bi-box-arrow-right fs-4 me-2"></i><span class="ms-1 d-none d-sm-inline">Cerrar Sesion</span>
+                                <i class="bi bi-box-arrow-right fs-4 me-2"></i><span
+                                    class="ms-1 d-none d-sm-inline text-white ">Cerrar Sesion</span>
                             </a>
+                        </li>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
                         </li>
                     </ul>
                 </div>
             </div>
-            <div class="col-md-9">
-            <div class="container mt-5">
-                <div style="margin-left: 100px;">
-                <h1>Bienvenido usuario</h1>
-                <div class="input-group mb-3">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text bg-white border-right-0"><i class="fas fa-search text-dark p-1"></i></span>
-                                </div>
-                                <input type="text" class="form-control border-left-0" placeholder="Buscar">
-                            </div>
-                            <div class="overflow-x-auto" style="max-height: 700px;">
-                            <div class="col-md-6">
-                                <div class="d-flex">
-                                    <img class="px-5" src="https://img.freepik.com/foto-gratis/vista-balon-futbol-campo_23-2150885897.jpg" alt="Imagen" class="img-fluid mt-3 mr-2" style="max-width: 750px;">
-                                    <img class="px-5" src="https://www.tudn.com/api/image/x/us/futbol" alt="Imagen" class="img-fluid mt-3 mr-2" style="max-width: 750px;">
-                                    <img class="px-5" src="https://images.pexels.com/photos/274422/pexels-photo-274422.jpeg?cs=srgb&dl=pexels-pixabay-274422.jpg&fm=jpg" alt="Imagen" class="img-fluid mt-3" style="max-width: 750px;">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
-    </div>
-</div>
+    </nav>
+            
 `;
 contenedorMenu.innerHTML = menuHTML;
 
