0c4f0dcc74b91a9fd7d043824a77894a643f2eb2
20220270 <20220270@ricaldone.edu.sv>
2024-04-14 18:39:58 -0600

    arreglos

---
+++ controllers/public/categorias_productos.js
@@ -0,0 +1,37 @@
+//popup del boton para cerrar ventana - imagen de comentarios
+document.getElementById('botonCerrarComen').addEventListener('mouseenter', function () {
+    var popover = new bootstrap.Popover(this, {
+        title: 'Cerrar ventana',
+        content: 'Haz clic aquí para cerrar este ventana. Puedes dar clic en nuestro logo para cerrar la ventana de igual forma',
+        placement: 'top',
+        trigger: 'manual',
+        boundary: 'viewport'
+    });
+    popover.show();
+});
+
+document.getElementById('botonCerrarComen').addEventListener('mouseleave', function () {
+    var popover = bootstrap.Popover.getInstance(this);
+    if (popover) {
+        popover.hide();
+    }
+});
+
+//popup del boton para cerrar ventana - logo de La Academia
+document.getElementById('botonCerrarComen2').addEventListener('mouseenter', function () {
+    var popover = new bootstrap.Popover(this, {
+        title: 'Cerrar ventana',
+        content: 'Haz clic aquí para cerrar este ventana. Puedes dar clic en la imagen de comentarios para cerrar la ventana de igual forma',
+        placement: 'top',
+        trigger: 'manual',
+        boundary: 'viewport'
+    });
+    popover.show();
+});
+
+document.getElementById('botonCerrarComen2').addEventListener('mouseleave', function () {
+    var popover = bootstrap.Popover.getInstance(this);
+    if (popover) {
+        popover.hide();
+    }
+});
