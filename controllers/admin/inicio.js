df2e6e0a9443abb38c60f92dfcabaa25cc666288
Riser245 <20220105@ricaldone.edu.sv>
2024-04-14 21:51:25 -0600

    prueba de que no ha muerto el repo :)

--- controllers/admin/inicio.js
+++ controllers/admin/inicio.js
@@ -2,85 +2,96 @@ const contenedorMenu = document.querySelector('#contenedor-menu');
 
 const menuHTML = `
 
-<link rel="stylesheet" href="/resources/css/privado/navbar.css">
+<link rel="stylesheet" href="/resources/css/privado/sidebar.css">
 
 
 <!-- Menú lateral -->
-<nav class="navbar navbar-dark bg-black fixed-top ">
-        <div class="container-fluid">
-            <img src="/resources/images/logoAcademiaBP.png" class="position-absolute top  end-0" alt="Logo" width=60px" height="60px">
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
-                aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar"
-                aria-labelledby="offcanvasNavbarLabel">
-                <div class="offcanvas-header bg-black">
-                    <img src="/resources/images/logoAcademiaBP.png" class="position-absolute top-0 start-50 translate-middle-x" alt="Logo" width="60px" height="60px">
-                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div class="offcanvas-body bg-black">
-                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3 mt-3">
-                        <li class="nav-item ">
-                            <a href="/views/admin/inicio.html" class="nav-link active bg-black mt-3" aria-current="page">
-                                <i class="bi bi-house-door fs-4 me-2"></i><span
-                                    class="ms-1 d-none d-sm-inline text-white">Inicio</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/views/admin/categorias_de_producto.html" class="nav-link active bg-black mt-3">
-                                <i class="bi bi-sliders fs-4 me-2"></i><span
-                                    class="ms-1 d-none d-sm-inline text-white">Categoria
-                                    de productos</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/views/admin/productos.html" class="nav-link active bg-black mt-3">
-                                <i class="fa-solid fa-money-bills fs-4 me-2"></i><span
-                                    class="ms-1 d-none d-sm-inline text-white">productos</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/views/admin/cancelacion_alumnos.html" class="nav-link active bg-black mt-3">
-                                <i class="fas fa-hand-holding-usd fs-4 me-2"></i><span
-                                    class="ms-1 d-none d-sm-inline text-white">Pagos de mensualidad</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="" class="nav-link active bg-black mt-3">
-                                <i class="bi bi-person-vcard fs-4 me-2"></i><span
-                                    class="ms-1 d-none d-sm-inline text-white">Alumnos</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="" class="nav-link active bg-black mt-3">
-                                <i class="bi bi-journal-text fs-4 me-2"></i><span
-                                    class="ms-1 d-none d-sm-inline text-white">Inscripciones</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="" class="nav-link active bg-black mt-3">
-                                <i class="fas fa-chalkboard-teacher fs-4 me-2"></i><span
-                                    class="ms-1 d-none d-sm-inline text-white">Profesores</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/views/admin/categorias_de_alumnos.html" class="nav-link active bg-black mt-3">
-                                <i class="fa-solid fa-table-cells fs-4 me-2"></i><span
-                                    class="ms-1 d-none d-sm-inline text-white">Categoria de alumnos</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="" class="nav-link active bg-black mt-4">
-                                <i class="bi bi-box-arrow-right fs-4 me-2"></i><span
-                                    class="ms-1 d-none d-sm-inline text-white ">Cerrar Sesion</span>
-                            </a>
-                        </li>
-                    </ul>
+<div class="wrapper">
+        <aside id="sidebar">
+
+            
+            <div class="d-flex">
+                <button class="toggle-btn" type="button">
+                    <img src="../../resources/images/logoAcademiaBP.png" alt="" width="30px" height="30px">
+                </button>
+                <div class="sidebar-logo">
+                    <a href="#">Menú</a>
                 </div>
             </div>
+            <ul class="sidebar-nav">
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="lni lni-user"></i>
+                        <span>inicio</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="lni lni-agenda"></i>
+                        <span>Categoria Productos</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="lni lni-agenda"></i>
+                        <span>Pagos dde Mensualidad</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="lni lni-agenda"></i>
+                        <span>Alumnos</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="lni lni-agenda"></i>
+                        <span>Inscripciones</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="lni lni-agenda"></i>
+                        <span>Categoria Productos</span>
+                    </a>
+                </li>
+
+
+
+               
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="lni lni-popup"></i>
+                        <span>Profesores</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="lni lni-cog"></i>
+                        <span>Entrenamientos</span>
+                    </a>
+                </li>
+            </ul>
+            <div class="sidebar-footer">
+                <a href="#" class="sidebar-link">
+                    <i class="lni lni-exit"></i>
+                    <span>Cerrar sesiòn</span>
+                </a>
+            </div>
+        </aside>
+        <div class="main p-3">
+            <div class="text-center">
+                <h1>
+                Categoria Productos
+                </h1>
+
+            </div>
         </div>
-    </nav>
+    </div>
 `;
 contenedorMenu.innerHTML = menuHTML;
 
