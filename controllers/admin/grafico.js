b2e5f928fbb41d69797b48a37a4ec7e890bb7516
Riser245 <20220105@ricaldone.edu.sv>
2024-04-18 22:57:35 -0600

    se agrego un segundo grafico

--- controllers/admin/grafico.js
+++ controllers/admin/grafico.js
@@ -5,7 +5,7 @@ const ctx = document.getElementById('graficoProductos');
 const data = {
     labels: ['Mochilas', 'Gorras', 'Botellas', 'Uniformes', 'Lapiceros', 'Chumpas'],
     datasets: [{
-      label: 'Weekly Sales',
+      label: 'Venta mensuales',
       data: [18, 12, 6, 9, 12, 3, 9],
       backgroundColor:'#408840',
     }]
@@ -29,6 +29,39 @@ const data = {
   const myChart = new Chart(
     document.getElementById('myChart'),
     config
+  );
+
+
+
+// setup 
+const data2 = {
+    labels: ['Sub20', 'Primera division', 'Sub17', 'Segunda division'],
+    datasets: [{
+      label: 'Categorias de alumnos',
+      data: [20, 14, 8, 6],
+      backgroundColor:'#ff',
+      color: '#408840'
+    }]
+  };
+
+  // config 
+  const config2 = {
+    type: 'bar',
+    data: data2,
+    options: {
+        maintainAspectRatio: false,
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  };
+
+  // render init block
+  const myChart2 = new Chart(
+    document.getElementById('myChart2'),
+    config2
   );
 
   // Instantly assign Chart.js version
