1d874953559f83aec89472330c1d2d85194d42a7
20220270 <136751152+20220270@users.noreply.github.com>
2024-06-05 11:59:17 -0600

    Pantallas ya con plantilla

--- controllers/admin/niveles_categorias.js
+++ controllers/admin/niveles_categorias.js
@@ -1,5 +1,8 @@
-const hamBurger = document.querySelector(".toggle-btn");
-
-hamBurger.addEventListener("click", function () {
-  document.querySelector("#sidebar").classList.toggle("expand");
+document.addEventListener('DOMContentLoaded', () => {
+  // Llamada a la función para mostrar el encabezado y pie del documento.
+  loadTemplate();
+  // Se establece el título del contenido principal.
+  //MAIN_TITLE.textContent = 'Gestionar categorías';
+  // Llamada a la función para llenar la tabla con los registros existentes.
+  //fillTable();
 });
--- controllers/admin/productos.js
+++ controllers/admin/productos.js
@@ -1,28 +1,8 @@
-// Función para cambiar el estado del comentario
-function cambiarEstadoComentario(event) {
-    // Seleccionar el elemento clicado
-    var comentario = event.target;
-    
-    // Verificar si el elemento clicado tiene la clase 'inactivo'
-    if (comentario.classList.contains('inactivo')) {
-        // Cambiar el texto del elemento a 'Activo'
-        comentario.textContent = 'Activo';
-        // Cambiar la clase a 'activo'
-        comentario.classList.remove('inactivo');
-        comentario.classList.add('activo');
-    } else {
-        // Cambiar el texto del elemento a 'Inactivo'
-        comentario.textContent = 'Inactivo';
-        // Cambiar la clase a 'inactivo'
-        comentario.classList.remove('activo');
-        comentario.classList.add('inactivo');
-    }
-}
-
-// Seleccionar todos los elementos con la clase 'comentario'
-var comentarios = document.querySelectorAll('.comentario');
-
-// Agregar un evento de clic a cada uno de los elementos seleccionados
-comentarios.forEach(function(comentario) {
-    comentario.addEventListener('click', cambiarEstadoComentario);
+document.addEventListener('DOMContentLoaded', () => {
+    // Llamada a la función para mostrar el encabezado y pie del documento.
+    loadTemplate();
+    // Se establece el título del contenido principal.
+    //MAIN_TITLE.textContent = 'Gestionar categorías';
+    // Llamada a la función para llenar la tabla con los registros existentes.
+    //fillTable();
 });
